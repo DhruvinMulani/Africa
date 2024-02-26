bc0111c3d2fb7985fbcc35ab0faf9d7c7989768b
DhruvinMulani <mulanidhruvin@gmail.com>
2024-02-25 20:06:39 -0500

    Initial Commit

---
+++ Africa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Dhruvin Mulani on 2024-02-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
